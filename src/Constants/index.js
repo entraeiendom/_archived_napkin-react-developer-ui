9dc46720ffbfcad063d8c7cc8f73418db46f2809
Thor Henning Hetland <totto@totto.org>
2020-06-30 22:02:43 +0200

    Update index.js

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,9 +1,9 @@
 export const APP_API_URL = 'https://api-demo.entraos.io';
 export const APP_ENVIRONMENT_NAME = 'demo';
 export const APP_GIT_SHA = 'sha';
-export const APP_BASE_URL = 'http://localhost:3000';
+export const APP_BASE_URL = 'https://api-demo.entraos.io';
 export const OAUTH_CLIENT_ID = 'aN6hrfyLwBl3PTnWKWWn_g--';
-export const OAUTH_DOMAIN= 'entrasso-devtest.entraos.io/oauth2'
+export const OAUTH_DOMAIN= 'entrasso-demo.entraos.io/oauth2'
 export const OAUTH_REDIRECT_URI = `${APP_BASE_URL}/callback`
 export const OAUTH_SCOPE = 'openid profile phone email'
 export const OAUTH_AUTO_RENEW_TOKEN = true
